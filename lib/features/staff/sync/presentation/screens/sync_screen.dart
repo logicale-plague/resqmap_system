8a8dff35adac7de52a08df360f8490ccc6d727e7
logicale-plague <militarytobi@gmail.com>
2026-04-01 23:39:45 +0800

    fixed comment

--- lib/features/staff/sync/presentation/screens/sync_screen.dart
+++ lib/features/staff/sync/presentation/screens/sync_screen.dart
@@ -52,7 +52,6 @@ class _SyncScreenState extends ConsumerState<SyncScreen> {
     final syncStatus = ref.watch(syncStatusProvider);
 
     return Scaffold(
-      // appBar: buildScreenAppBar(title: 'Sync Status'),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(24),
         child: Column(
